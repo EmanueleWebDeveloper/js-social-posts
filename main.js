bf036464408a7ed3cae5ab84cdbe71a5fcffbdab
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-01-23 17:39:22 +0100

    like blu

--- main.js
+++ main.js
@@ -77,39 +77,53 @@ const posts = [
 ];
 
 
-const containerHtml = document.getElementById("container")
+const containerHtml = document.getElementById("container");
 
-                                        // sostituzione html con js
+function handleLikeButtonClick(postId) {
+    const likeButton = document.querySelector(`[data-postid="${postId}"]`);
+    const likeCounter = document.getElementById(`like-counter-${postId}`);
+    const likeLabel = document.querySelector(`#like-label-${postId}`);
+
+    
+    likeButton.classList.toggle("liked");
+    likeLabel.classList.toggle("liked-label"); 
+
+    // Aggiorna il conteggio dei Mi Piace
+    const currentLikes = parseInt(likeCounter.textContent);
+    likeCounter.textContent = likeButton.classList.contains("liked") ? currentLikes + 1 : currentLikes - 1;
+}
+
+// genera l'HTML
 posts.forEach((element) => {
-
-    containerHtml.innerHTML += `<div class="post">
-                                    <div class="post__header">
-                                        <div class="post-meta">
-                                            <div class="post-meta__icon">
-                                                <img class="profile-pic" src="${element.author.image}" alt="Phil Mangione">
-                                            </div>
-                                            <div class="post-meta__data">
-                                                <div class="post-meta__author">${element.author.name}</div>
-                                                <div class="post-meta__time">${element.created}</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="post__text">${element.content}</div>
-                                    <div class="post__image">
-                                        <img src="${element.media}" alt="">
-                                    </div>
-                                    <div class="post__footer">
-                                        <div class="likes js-likes">
-                                            <div class="likes__cta">
-                                                <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
-                                                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                                                    <span class="like-button__label">Mi Piace</span>
-                                                </a>
-                                            </div>
-                                            <div class="likes__counter">
-                                                Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>`
+    containerHtml.innerHTML += `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${element.author.image}" alt="Profile Pic">
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${element.author.name}</div>
+                        <div class="post-meta__time">${element.created}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="post__text">${element.content}</div>
+            <div class="post__image">
+                <img src="${element.media}" alt="Post Image">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button js-like-button" href="#" data-postid="${element.id}" onclick="handleLikeButtonClick(${element.id})">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span id="like-label-${element.id}" class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
+                    </div>
+                </div>
+            </div>
+        </div>`;
 });
